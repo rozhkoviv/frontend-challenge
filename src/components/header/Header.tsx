24571bf0822153c4b3657a89ecb9daec3a034c69
rozhkoviv <rozhkoviv@outlook.com>
2022-06-09 13:24:10 +0300

    added homepage option

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -7,10 +7,10 @@ export default function Header () {
   return (
     <div className='header'>
       <div className='menu'>
-        <Link to={'/frontend-challenge/'} className={location.pathname === '/frontend-challenge/'?'menu-item selected':'menu-item'}>
+        <Link to={'/'} className={location.pathname === '/'?'menu-item selected':'menu-item'}>
           Все котики
         </Link>
-        <Link to={'/frontend-challenge/favourite'} className={location.pathname === '/frontend-challenge/favourite'?'menu-item selected':'menu-item'}>
+        <Link to={'/favourite'} className={location.pathname === '/favourite'?'menu-item selected':'menu-item'}>
           Любимые котики
         </Link>
       </div>
